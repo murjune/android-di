a0bcdc8ee1216771df432b3a8eb90307ca4db198
kkosang <eiwoqp77@gmail.com>
2024-10-02 14:43:24 +0900

    feat: Application에서 Hilt 사용

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,8 +1,10 @@
 package woowacourse.shopping
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import woowacourse.shopping.data.di.RepositoryModule
 
+@HiltAndroidApp
 class ShoppingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
