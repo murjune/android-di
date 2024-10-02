e38eb2943ac716055fbcfe70be304653a4e06a1b
kkosang <eiwoqp77@gmail.com>
2024-10-02 14:42:33 +0900

    feat: Hilt 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -78,4 +79,7 @@ dependencies {
     testImplementation("org.robolectric:robolectric:4.13")
     // Di
     implementation(project(":di"))
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
 }
