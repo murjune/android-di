f26039a25bd9806e95c2891abbd8e4009355988a
kkosang <eiwoqp77@gmail.com>
2024-10-02 18:00:29 +0900

    feat: DateFormatter ActivityContext 주입

--- app/src/main/java/woowacourse/shopping/ui/cart/DateFormatter.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/DateFormatter.kt
@@ -1,14 +1,14 @@
 package woowacourse.shopping.ui.cart
 
 import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.qualifiers.ActivityContext
 import woowacourse.shopping.R
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
 
-class DateFormatter @Inject constructor(@ApplicationContext context: Context) {
+class DateFormatter @Inject constructor(@ActivityContext context: Context) {
     private val formatter =
         SimpleDateFormat(
             context.getString(R.string.date_format),
