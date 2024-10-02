82c12f15c4cd0f57e5469bea5b18930ca17ef9f3
murjune <sis9221302@gmail.com>
2024-10-02 15:53:18 +0900

    koin unit test

--- koin-app/build.gradle.kts
+++ koin-app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     val koin = "4.0.0"
     implementation(platform("io.insert-koin:koin-bom:$koin"))
     implementation("io.insert-koin:koin-android")
+    testImplementation("io.insert-koin:koin-test-junit5")
+    testImplementation("io.insert-koin:koin-android-test")
     // android
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -64,19 +66,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("javax.inject:javax.inject:1")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("com.google.truth:truth:1.1.3")
-    // Reflection
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
@@ -84,12 +73,24 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
     // Robolectric
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.13")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.test.ext:junit-ktx:1.2.1")
+    testImplementation("androidx.test:core-ktx:1.6.1")
+    // Mockk
+    testImplementation("io.mockk:mockk:1.12.2")
+    // junit4
+    testImplementation("junit:junit:4.13.2")
     // junit5 & kotest
+    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("de.mannodermaus.junit5:android-test-runner:1.4.0")
+    // android test
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 kapt {
