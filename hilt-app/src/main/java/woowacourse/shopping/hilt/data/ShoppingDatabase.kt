f5e450521d2f51188b59c31259e78fb8d12081b3
murjune <sis9221302@gmail.com>
2024-10-03 21:15:06 +0900

    ktFormat

--- hilt-app/src/main/java/woowacourse/shopping/hilt/data/ShoppingDatabase.kt
+++ hilt-app/src/main/java/woowacourse/shopping/hilt/data/ShoppingDatabase.kt
@@ -2,8 +2,6 @@ package woowacourse.shopping.hilt.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import woowacourse.shopping.hilt.data.CartProductDao
-import woowacourse.shopping.hilt.data.CartProductEntity
 
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
